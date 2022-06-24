d3fe04bbfbcd3653ea0e0ed7c27cf50d3b74a1bc
MikeJoe789 <108030958+MikeJoe789@users.noreply.github.com>
2022-06-24 11:42:19 +0100

    upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    if (principal > 0){
+        document.getElementById("result").innerHTML = "Interest : <br>If you deposit:  " + "<span style='background-color:yellow'>" + principal + "</span>" + "<br>at an interest rate of:   " + "<span style='background-color:yellow'>" + rate + "</span>" + "<br> You will receive an amount of:   " + "<span style='background-color:yellow'>" +  interest +"</span>"+ "<br>in the year:  "+ "<span style='background-color:yellow'>"  + year + "</span>"
+    } else {
+        alert("Enter a positive number")
+        document.getElementById("principal").focus();
+    }
+
+
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}       
